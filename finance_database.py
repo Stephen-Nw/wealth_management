86f318ca922964b6d3bae8fd3b6b4a02a5a4a457
Stephen Nwaukwa <interprimos80@hotmail.com>
2021-09-12 00:29:46 -0500

    create add new user function

--- finance_database.py
+++ finance_database.py
@@ -3,6 +3,7 @@ import sqlite3
 db = sqlite3.connect("finance-db")
 cursor = db.cursor()
 
+
 # cursor.execute(
 #     "CREATE TABLE user (username varchar(250) NOT NULL UNIQUE,"
 #     "password varchar(250) NOT NULL UNIQUE,"
@@ -13,3 +14,11 @@ cursor = db.cursor()
 #     "question_three varchar(250),"
 #     "answer_three varchar(250))"
 # )
+
+def add_new_user(name, pword, q1, a1, q2, a2, q3, a3):
+    cursor.execute("INSERT INTO user VALUES (:user, :password, :question_one, :answer_one, :question_two, "
+                   ":answer_two, question_three, answer_three)", {"user": name, "password": pword, "question_one": q1,
+                                                                  "answer_one": a1,
+                                                                  "question_two": q2, "answer_two": a2,
+                                                                  "question_three": q3, "answer_three": a3})
+    db.commit()
