4718cb5f7310e757222a11f530472b0d4ac65ff0
Stephen Nwaukwa <interprimos80@hotmail.com>
2021-10-15 21:53:06 -0500

    create income table & add_new_income fxn

--- finance_database.py
+++ finance_database.py
@@ -3,7 +3,6 @@ import sqlite3
 db = sqlite3.connect("finance-db")
 cursor = db.cursor()
 
-
 # cursor.execute(
 #     "CREATE TABLE user (username varchar(250) NOT NULL UNIQUE,"
 #     "password varchar(250) NOT NULL UNIQUE,"
@@ -22,15 +21,22 @@ cursor = db.cursor()
 #     "amount varchar(250))"
 # )
 
+# cursor.execute(
+#     "CREATE TABLE income (date varchar(250),"
+#     "category varchar(250),"
+#     "amount varchar(250))"
+# )
+
+
 def add_new_expense(dt, cat, itm, amt):
-    """Add new expense to database"""
+    """Add new expense to expense table"""
     cursor.execute("INSERT INTO expense VALUES (:date, :category, :item, :amount)",
                    {"date": dt, "category": cat, "item": itm, "amount": amt})
     db.commit()
 
 
 def add_new_user(name, pword, q1, a1, q2, a2, q3, a3):
-    """Add new user to database"""
+    """Add new user to user table"""
     cursor.execute("INSERT INTO user VALUES (:user, :password, :question_one, :answer_one, :question_two, "
                    ":answer_two, :question_three, :answer_three)", {"user": name, "password": pword, "question_one": q1,
                                                                     "answer_one": a1,
@@ -40,15 +46,20 @@ def add_new_user(name, pword, q1, a1, q2, a2, q3, a3):
 
 
 def retrieve_user_info(name):
-    """Retrieve specific user information from database using username"""
+    """Retrieve specific user information from user table using username"""
     cursor.execute("SELECT * FROM user WHERE username=?", (name,))
     user_info = cursor.fetchall()
     return user_info
 
 
 def update_user_password(name, p_word):
-    """Update specific user password in database"""
+    """Update specific user password in user table"""
     cursor.execute("UPDATE user SET password = ? WHERE username = ?", (p_word, name))
     db.commit()
 
 
+def add_new_income(dt, cat, amt):
+    """Add new income to income table"""
+    cursor.execute("INSERT INTO expense VALUES (:date, :category, :item, :amount)",
+                   {"date": dt, "category": cat, "amount": amt})
+    db.commit()
