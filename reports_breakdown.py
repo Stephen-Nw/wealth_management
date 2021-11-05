3183ba09089e7b591057ceaf17517f797fd32515
Stephen Nwaukwa <interprimos80@hotmail.com>
2021-11-04 22:29:24 -0500

    create monthly report

--- reports_breakdown.py
+++ reports_breakdown.py
@@ -2,7 +2,6 @@ from tkinter import *
 from tkinter import ttk
 from tkinter.font import Font
 
-
 root = Tk()
 root.title("Reports")
 main_frame = ttk.Frame(root, padding=10, width=950, height=350)
@@ -20,9 +19,14 @@ def yearly_breakdown():
     pass
 
 
+def monthly_report():
+    pass
+
+
 # ************* CONSTANTS *******************
 summary_text = "Yearly Summary"
 breakdown_text = "Yearly Breakdown"
+monthly_text = "Monthly Reports"
 
 year = []
 for num in range(0, 11):
@@ -30,7 +34,6 @@ for num in range(0, 11):
 
 month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
          "November", "December"]
-
 
 # ************* Yearly Summary ******************
 summary_label = ttk.Label(main_frame, text=summary_text, font=label_font, anchor="center", padding=5)
@@ -58,8 +61,23 @@ selected_year2.grid(row=1, column=1, padx=5, pady=5)
 breakdown_button = ttk.Button(main_frame, text="Submit", command=yearly_breakdown)
 breakdown_button.grid(row=1, column=2, padx=5, pady=5)
 
+# ************* Monthly report ******************
+monthly_label = ttk.Label(main_frame, text=monthly_text, font=label_font, anchor="center", padding=5)
+monthly_label.grid(row=2, column=0)
 
+choose_year3 = StringVar()
+choose_year3.set("Year")
+selected_year3 = ttk.Combobox(main_frame, textvariable=choose_year3, values=year, state="readonly",
+                              width=5, justify="center")
+selected_year3.grid(row=2, column=1, padx=5, pady=5)
 
+choose_month = StringVar()
+choose_month.set("Month")
+selected_month = ttk.Combobox(main_frame, textvariable=choose_month, values=month, state="readonly",
+                              width=10, justify="center")
+selected_month.grid(row=2, column=2, padx=5, pady=5)
+
+month_button = ttk.Button(main_frame, text="Submit", command=monthly_report)
+month_button.grid(row=2, column=3, padx=5, pady=5)
 
 root.mainloop()
-
