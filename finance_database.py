5dfdfc5fb5f412c51992ba300e716c87cd762339
Stephen Nwaukwa <interprimos80@hotmail.com>
2021-11-08 23:42:36 -0600

    code cleanup

--- finance_database.py
+++ finance_database.py
@@ -231,47 +231,9 @@ def financial_summary():
     def go_back():
         pass
 
-    heading_font = Font(family="Helvetica", size=10, weight="bold")
-    label_font = Font(family="Helvetica", size=8, weight="bold")
-
-    heading_text = "Financial Summary"
-    income_text = "Yearly Income"
-    expense_text = "Yearly Expenses"
-    savings_text = "Yearly Savings"
-    investment_text = "Yearly Investments"
-
-    # ****************** CREATE FINANCIAL SUMMARY STRUCTURE *************************
-    heading_label = ttk.Label(main_frame, text=heading_text, font=heading_font, anchor="center", padding=5)
-    heading_label.grid(row=0, column=0, columnspan=2, padx=5)
-
-    income_label = ttk.Label(main_frame, text=income_text, font=label_font)
-    income_label.grid(row=1, column=0, pady=5, padx=5)
-
-    income_amount = ttk.Label(main_frame, text="blank1", font=label_font)
-    income_amount.grid(row=1, column=1, pady=5, padx=5)
-
-    expense_label = ttk.Label(main_frame, text=expense_text, font=label_font)
-    expense_label.grid(row=2, column=0, pady=5, padx=5)
-
-    expense_amount = ttk.Label(main_frame, text="blank2", font=label_font)
-    expense_amount.grid(row=2, column=1, padx=5)
-
-    savings_label = ttk.Label(main_frame, text=savings_text, font=label_font)
-    savings_label.grid(row=3, column=0, pady=5, padx=5)
-
-    savings_amount = ttk.Label(main_frame, text="blank3", font=label_font)
-    savings_amount.grid(row=3, column=1, padx=5)
-
-    investment_label = ttk.Label(main_frame, text=investment_text, font=label_font)
-    investment_label.grid(row=4, column=0, pady=5, padx=5)
-
-    investment_amount = ttk.Label(main_frame, text="blank4", font=label_font)
-    investment_amount.grid(row=4, column=1, padx=5)
-
-    back_btn = ttk.Button(main_frame, text="Back", command=go_back)
-    back_btn.grid(row=5, column=0, columnspan=2, pady=5)
-
-    # ************** RETRIEVE DATA FROM DATABASE ***********************************
+    # ****************************************************************************** #
+    #                RETRIEVE DATA FROM DATABASE                                     #
+    # ****************************************************************************** #
     expense_df = pd.read_sql_query("SELECT * from expense", db)
     income_df = pd.read_sql_query("SELECT * from income", db)
     savings_df = pd.read_sql_query("SELECT * from savings", db)
@@ -316,13 +278,13 @@ def financial_summary():
     requested_year_investments = investments_df['year'] == requested_year
     requested_year_investments_df = investments_df[requested_year_investments]
 
-    print(requested_year_expense_df)
-    print("=========================================")
-    print(requested_year_income_df)
-    print("=========================================")
-    print(requested_year_savings_df)
-    print("=========================================")
-    print(requested_year_investments_df)
+    # print(requested_year_expense_df)
+    # print("=========================================")
+    # print(requested_year_income_df)
+    # print("=========================================")
+    # print(requested_year_savings_df)
+    # print("=========================================")
+    # print(requested_year_investments_df)
 
     income_sum = requested_year_income_df["amount"].sum()
     print(income_sum)
@@ -341,6 +303,53 @@ def financial_summary():
     investments_withdraw = requested_year_investments_df["withdraw"].sum()
     investments_balance = investments_deposit - investments_withdraw
     print(investments_balance)
+
+    heading_font = Font(family="Helvetica", size=10, weight="bold")
+    label_font = Font(family="Helvetica", size=8, weight="bold")
+
+    heading_text = "Financial Summary"
+    income_text = "Yearly Income"
+    expense_text = "Yearly Expenses"
+    savings_text = "Yearly Savings"
+    investment_text = "Yearly Investments"
+
+    # ****************************************************************************** #
+    #          CREATE FINANCIAL SUMMARY STRUCTURE                                    #
+    # ****************************************************************************** #
+    heading_label = ttk.Label(main_frame, text=heading_text, font=heading_font, anchor="center", padding=5)
+    heading_label.grid(row=0, column=0, columnspan=2, padx=5)
+
+    income_label = ttk.Label(main_frame, text=income_text, font=label_font)
+    income_label.grid(row=1, column=0, pady=5, padx=5)
+
+    income_amount = ttk.Label(main_frame, text=income_sum, font=label_font)
+    income_amount.grid(row=1, column=1, pady=5, padx=5)
+
+    expense_label = ttk.Label(main_frame, text=expense_text, font=label_font)
+    expense_label.grid(row=2, column=0, pady=5, padx=5)
+
+    expense_amount = ttk.Label(main_frame, text=expense_sum, font=label_font)
+    expense_amount.grid(row=2, column=1, padx=5)
+
+    savings_label = ttk.Label(main_frame, text=savings_text, font=label_font)
+    savings_label.grid(row=3, column=0, pady=5, padx=5)
+
+    savings_amount = ttk.Label(main_frame, text=savings_balance, font=label_font)
+    savings_amount.grid(row=3, column=1, padx=5)
+
+    investment_label = ttk.Label(main_frame, text=investment_text, font=label_font)
+    investment_label.grid(row=4, column=0, pady=5, padx=5)
+
+    investment_amount = ttk.Label(main_frame, text=investments_balance, font=label_font)
+    investment_amount.grid(row=4, column=1, padx=5)
+
+    back_btn = ttk.Button(main_frame, text="Back", command=go_back)
+    back_btn.grid(row=5, column=0, columnspan=2, pady=5)
+
+
+
+    # TODO 1: Use get request to capture year for yearly summary
+
 
 
 
